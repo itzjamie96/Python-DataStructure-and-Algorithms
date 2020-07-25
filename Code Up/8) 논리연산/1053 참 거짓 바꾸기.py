5fbbecfbe98fba42ea158cc4ff942cc1e751c552
Jamie Lee <itzjamie96@gmail.com>
2020-07-25 12:13:46 +0900

    [ADD] CodeUp 논리연산 1053 ~ 1058 삼항연산 활용

---
+++ Code Up/8) 논리연산/1053 참 거짓 바꾸기.py
@@ -0,0 +1,5 @@
+# 1(true, 참) 또는 0(false, 거짓) 이 입력되었을 때
+# 반대로 출력하는 프로그램을 작성해보자.
+
+n = int(input())
+print(1 if n==False else 0)
---
+++ Code Up/8) 논리연산/1058 둘 다 거짓일 때만 참 출력하기.py
@@ -0,0 +1,2 @@
+a, b = map(int, input().split())
+print(1 if a==False and b==False else 0)
